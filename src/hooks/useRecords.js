17b05f8dbeff38095484b9205c7ad0a0f7a8640a
Joel Bartlett <joel@murbar.com>
2019-03-15 21:18:10 -0400

    Store and log history of state

--- src/hooks/useRecords.js
+++ src/hooks/useRecords.js
@@ -1,11 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import faker from 'faker';
 
 const useRecords = () => {
   const [state, setState] = useState([]);
+  const [history, setHistory] = useState([
+    {
+      state,
+      action: 'Initial state'
+    }
+  ]);
+
+  useEffect(() => {
+    const historyCount = history.length;
+    const lastAction = history[historyCount - 1];
+    console.log(lastAction);
+  }, [history]);
+
+  const appendToHistory = (actionLabel, actionPayload) => {
+    setHistory(prev => [
+      ...prev,
+      {
+        state,
+        action: `${actionLabel} ${actionPayload}`
+      }
+    ]);
+  };
 
   const add = item => {
+    item.id = faker.random.uuid();
     setState(prev => [...prev, item]);
-    console.log('ADD', item);
+    appendToHistory('ADD', item.id);
   };
 
   const update = item => {
@@ -15,12 +39,12 @@ const useRecords = () => {
         return item;
       })
     );
-    console.log('UPDATE', item);
+    appendToHistory('UPDATE', item.id);
   };
 
   const remove = itemId => {
     setState(prev => prev.filter(i => i.id !== itemId));
-    console.log('REMOVE', itemId);
+    appendToHistory('REMOVE', itemId);
   };
 
   return {
