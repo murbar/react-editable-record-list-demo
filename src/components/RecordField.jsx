141cc5e01bc06f420cc3967d599b4df886349ee2
Joel Bartlett <joel@murbar.com>
2019-03-15 22:15:32 -0400

    Render record table cell

--- src/components/RecordField.jsx
+++ src/components/RecordField.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const RecordField = () => {
-  return <div>RecordField</div>;
+const StyledCell = styled.td`
+  padding: 0.25rem 0.5rem;
+  border-right: 1px solid #666;
+  &:last-child {
+    border-right: none;
+  }
+`;
+
+const RecordField = ({ data }) => {
+  return <StyledCell>{data}</StyledCell>;
 };
 
 export default RecordField;
