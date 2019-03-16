08e43e09d0cb2e6207d4e63ed47b0181b7f98652
Joel Bartlett <joel@murbar.com>
2019-03-15 22:15:18 -0400

    Render record table row

--- src/components/Record.jsx
+++ src/components/Record.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+import RecordField from './RecordField';
 
-const Record = () => {
-  return <div>Record</div>;
+const StyledRow = styled.tr`
+  border-top: 1px solid #666;
+  border-right: 1px solid #666;
+  border-left: 1px solid #666;
+  &:last-child {
+    border-bottom: 1px solid #666;
+  }
+`;
+
+const Record = ({ data }) => {
+  return (
+    <StyledRow>
+      {Object.keys(data).map((key, i) => (
+        <RecordField key={i} data={data[key]} />
+      ))}
+    </StyledRow>
+  );
 };
 
 export default Record;
